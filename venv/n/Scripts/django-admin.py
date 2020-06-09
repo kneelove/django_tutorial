f7467582226f26ea49aae105a28537b87523534b
kneelove <nilav.vaghasiya@gmail.com>
2020-06-08 20:17:49 -0500

    Initial Commit

---
+++ venv/n/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\nilav\PycharmProjects\django_tutorial\venv\n\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
